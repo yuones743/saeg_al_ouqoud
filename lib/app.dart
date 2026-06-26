0a6e65eca68ebd662c3261f6b1cd027c415f1b74
yuones743 <yuones00001991@gmail.com>
2026-06-26 21:44:31 +0300

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -51,60 +51,8 @@ class SaegApp extends StatelessWidget {
         titleMedium: TextStyle(fontFamily: SystemConfig.appFontFamily),
       ),
       inputDecorationTheme: const InputDecorationTheme(
-        border switch (_contractFont) {
-      case ContractFontFamily.traditionalArabic: return 'Traditional Arabic';
-      case ContractFontFamily.amiri: return 'Amiri';
-      case ContractFontFamily.cairo: return 'Cairo';
-      case ContractFontFamily.notoNaskh: return 'Noto Naskh Arabic';
-      case ContractFontFamily.lateef: return 'Lateef';
-    }
-  }
-
-  static String get contractFontAssetPath {
-    switch (_contractFont) {
-      case ContractFontFamily.traditionalArabic: return 'assets/fonts/TraditionalArabic.ttf';
-      case ContractFontFamily.amiri: return 'assets/fonts/Amiri-Regular.ttf';
-      case ContractFontFamily.cairo: return 'assets/fonts/Cairo-Regular.ttf';
-      case ContractFontFamily.notoNaskh: return 'assets/fonts/NotoNaskhArabic-Regular.ttf';
-      case ContractFontFamily.lateef: return 'assets/fonts/Lateef-Regular.ttf';
-    }
-  }
-
-  static String get contractFontBoldAssetPath {
-    switch (_contractFont) {
-      case ContractFontFamily.traditionalArabic: return 'assets/fonts/TraditionalArabic.ttf';
-      case ContractFontFamily.amiri: return 'assets/fonts/Amiri-Bold.ttf';
-      case ContractFontFamily.cairo: return 'assets/fonts/Cairo-Bold.ttf';
-      case ContractFontFamily.notoNaskh: return 'assets/fonts/NotoNaskhArabic-Bold.ttf';
-      case ContractFontFamily.lateef: return 'assets/fonts/Lateef-Regular.ttf';
-    }
-  }
-
-  static void setContractFont(ContractFontFamily font) {
-    _contractFont = font;
-    _savePrefs();
-  }
-
-  static void setPageFormat(ContractPageFormat fmt) {
-    _pageFormat = fmt;
-    _savePrefs();
-  }
-
-  static Future<void> load() async {
-    try {
-      final prefs = await SharedPreferences.getInstance();
-      final fontIdx = prefs.getInt('contract_font') ?? 0;
-      final fmtIdx = prefs.getInt('page_format') ?? 0;
-      _contractFont = ContractFontFamily.values[fontIdx.clamp(0, ContractFontFamily.values.length - 1)];
-      _pageFormat = ContractPageFormat.values[fmtIdx.clamp(0, ContractPageFormat.values.length - 1)];
-    } catch (_) {}
-  }
-
-  static Future<void> _savePrefs() async {
-    try {
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setInt('contract_font', _contractFont.index);
-      await prefs.setInt('page_format', _pageFormat.index);
-    } catch (_) {}
+        border: OutlineInputBorder(),
+      ),
+    );
   }
 }
